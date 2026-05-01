5cce52bd456a0786a70c1d2dfa0f4e7219322a1f
aarokan <aarokan@gmail.com>
2026-05-01 21:17:31 +0300

    Overflow: explore unsigned int maximmum value

--- 6_1_Overflow.c
+++ 6_1_Overflow.c
@@ -3,6 +3,7 @@
     Max for negative is -2^31 = -2,147,483,648
     Max for positve is 2^31-1 = 2,147,483,647
     If you reach max it will wrap around to negative (vice versa) or Zero
+    Note: unsigned int max value is 2^32-1 (postive takes up full 4 bytes)
 */
 
 #include <cs50.h>
@@ -11,9 +12,12 @@
 int main(void)
 {
     int dollars = 1;
+    // unsigned int dollars = 4294967295;
     while (true)
     {
         char c = get_char("Here's $%i. Double it for the next person? ", dollars);
+        // use %u for unsigned int
+        // char c = get_char("Here's $%u. Double it for the next person? ", dollars);
         if (c == 'y')
         {
             dollars *= 2;
@@ -24,7 +28,7 @@ int main(void)
         }
         else if (c == '2') // to help you wrap around from negatvie to max positive
         {
-            dollars --;
+            dollars--;
         }
         else
         {
@@ -32,4 +36,5 @@ int main(void)
         }
     }
     printf("Here's $%i\n", dollars);
+    // printf("Here's $%u\n", dollars);  // used for unsigned int
 }
