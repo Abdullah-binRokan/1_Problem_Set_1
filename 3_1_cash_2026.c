bce45776352aa67de49fd27fbcaf4109bdd02762
aarokan <aarokan@gmail.com>
2026-05-05 00:14:15 +0300

    Cash - min num of coins: Show minmmum numbers of coins

--- 3_1_cash_2026.c
+++ 3_1_cash_2026.c
@@ -1,5 +1,5 @@
-// Customer gives you $1.00 for item costs $0.50
-// Show minmmum nummber of coins to Pay their change
+// When customer gives you $1.00 for item costs $0.40
+// Show minmmum nummber of coins to pay their change e.g., 3
 /*  
     Apply Greedy Algorithms to minimize numbers of coins
     In cashier’s drawer are quarters, dimes, nickels and pennies 
@@ -8,6 +8,9 @@
 
 #include <cs50.h>
 #include <stdio.h>
+
+// define function prototype
+int min_number_coins(int change_owed);
 
 int main(void)
 {
@@ -18,16 +21,37 @@ int main(void)
     }
     while (change_owed < 0);
 
-    printf("%i", min_number_coins(change_owed));
+    printf("%i\n", min_number_coins(change_owed));
 }
 
 
-int min_number_coins(change_owed)
+int min_number_coins(int change_owed)
 {
     // initilize coins as constant -TODO refactor to struct or enum
-    // while change_owed > 0
-        // if change_owed > QUARTERS
-            // change_owed - QUARTERS
-        // else if ... 
-}
+    const int QUARTERS = 25, DIMES = 10, NICKELS = 5, PENNIES = 1;
+    int coins_counter = 0;
 
+    // while change_owed > 0
+    while (change_owed > 0)
+    {
+        if (change_owed >= QUARTERS)
+        {
+            change_owed -= QUARTERS;
+        }
+        else if (change_owed >= DIMES)
+        {
+            change_owed -= DIMES;
+        }
+        else if (change_owed >= NICKELS)
+        {
+            change_owed -= NICKELS;
+        }
+        else 
+        {
+            change_owed -= PENNIES;
+        }
+        
+        coins_counter++;
+    }
+    return coins_counter;
+}
